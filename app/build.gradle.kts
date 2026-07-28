6f92a6c4ef6141d89e51a23b56bd2545545652fb
Rami Del Toro <rami.deltoro@gmail.com>
2026-07-28 00:05:17 -0400

    Bump Android version code to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         applicationId = "com.nutsnews.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
